5d8bbe698b29c0e54caac81772032de80bf49522
renmurakami <autoeyy@gmail.com>
2022-09-29 12:14:46 +0900

    mokey rotate

--- src/components/model.tsx
+++ src/components/model.tsx
@@ -1,11 +1,16 @@
 import { useGLTF } from "@react-three/drei";
-import { FC } from "react";
+import { useFrame } from "@react-three/fiber";
+import { FC, useRef } from "react";
+import { Group } from "three";
 
 const Model: FC = () => {
+	const modelRef = useRef<Group>(null!);
 	const { scene } = useGLTF("/model/monkey.glb");
 
+	useFrame(() => (modelRef.current.rotation.y += 0.01));
+
 	return (
-		<group dispose={null}>
+		<group dispose={null} ref={modelRef}>
 			<primitive scale={[1, 1, 1]} object={scene} />
 		</group>
 	);
