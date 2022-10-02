fca81d7743a158cfe5d693cee96fedc69960c31c
renmurakami <autoeyy@gmail.com>
2022-10-02 18:10:24 +0900

    dynamic color change

--- src/components/sceneWrap.tsx
+++ src/components/sceneWrap.tsx
@@ -2,11 +2,12 @@ import { FC, ReactNode, Suspense } from "react";
 
 type SceneWrapProps = {
 	children: ReactNode;
+	height?: string;
 };
 
-const SceneWrap: FC<SceneWrapProps> = ({ children }) => {
+const SceneWrap: FC<SceneWrapProps> = ({ children, height }) => {
 	return (
-		<div style={{ width: "100vw", height: "100vh" }}>
+		<div style={{ width: "100vw", height: height ? height : "100vh" }}>
 			<Suspense fallback={<h1>loading...</h1>}>{children}</Suspense>
 		</div>
 	);
--- src/pages/shoeScene.tsx
+++ src/pages/shoeScene.tsx
@@ -1,18 +1,25 @@
 import { OrbitControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { FC } from "react";
+import { FC, useId, useState } from "react";
 import SceneWrap from "../components/sceneWrap";
 import { Shoe } from "../components/Shoe";
 
 const ShoeScene: FC = () => {
+	const id = useId();
+	const [color, setColor] = useState("");
+
 	return (
-		<SceneWrap>
+		<SceneWrap height="85vh">
 			<Canvas>
 				<Stage>
-					<Shoe shoeColors={{ laces: "red", mess: "skyblue", caps: "#333" }} />
+					<Shoe shoeColors={{ laces: "#333", mess: color, caps: "#333" }} />
 				</Stage>
 				<OrbitControls />
 			</Canvas>
+			<div>
+				<label htmlFor={id + "color-input"}>color change test</label>
+				<input type="color" name="color" id={id + "color-input"} onChange={(e) => setColor(e.currentTarget.value)} />
+			</div>
 		</SceneWrap>
 	);
 };
